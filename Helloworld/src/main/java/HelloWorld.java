a6519111a8c124cfec66e50a76ba9c21115bec7d
khotso-26 <70955464+khotso-26@users.noreply.github.com>
2022-05-16 21:10:34 +0200

    hello world DONE!

--- Helloworld/src/main/java/HelloWorld.java
+++ Helloworld/src/main/java/HelloWorld.java
@@ -1,5 +1,32 @@
+
+import java.util.Scanner;
+
 public class HelloWorld {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+    private final String string;
+
+    public HelloWorld(String string) {
+        this.string = string;
     }
+
+    public String getName() {
+        return this.string;
+    }
+
+    public String sayHello(){
+        return "Hello " + getName().substring(0,1).toUpperCase()
+                + getName().substring(1);
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Replace the word 'World' in [Hello World] with any word of your choice: ");
+        String word = scanner.nextLine();
+
+        HelloWorld helloWorld = new HelloWorld(word);
+        System.out.println(helloWorld.sayHello());
+
+    }
+
+
 }
---
+++ Helloworld/src/test/java/HelloWorldTest.java
@@ -0,0 +1,20 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HelloWorldTest {
+
+    @Test
+    public void testingHelloWorld(){
+        String word = "world";
+        HelloWorld helloWorld = new HelloWorld(word);
+        assertEquals(helloWorld.sayHello(), "Hello World");
+    }
+
+    @Test
+    public void testingHelloWorldWithName(){
+        String word = "felicia";
+        HelloWorld helloWorld = new HelloWorld(word);
+        assertEquals(helloWorld.sayHello(), "Hello Felicia");
+    }
+}
